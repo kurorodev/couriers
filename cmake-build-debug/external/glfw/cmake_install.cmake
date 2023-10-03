1f3e3a42431217ab1f0eba597c3c2691ddb8b946
kurorodev <danyakud13@gmail.com>
2023-10-03 17:55:34 +0400

    glfw

--- cmake-build-debug/external/glfw/cmake_install.cmake
+++ cmake-build-debug/external/glfw/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/danyadevpc/Desktop/couriers
+# Install script for directory: C:/Users/danyadevpc/Desktop/couriers/external/glfw
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,16 +39,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/danyadevpc/Desktop/couriers/cmake-build-debug/external/glfw/cmake_install.cmake")
+  include("C:/Users/danyadevpc/Desktop/couriers/cmake-build-debug/external/glfw/src/cmake_install.cmake")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/danyadevpc/Desktop/couriers/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
